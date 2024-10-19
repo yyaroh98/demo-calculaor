42c593983a7389e258bb1d1c9653e12df95362dd
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-19 15:14:58 +0300

    add file CalculatorTest

---
+++ src/main/java/by/itacademy/eugeneyarosh/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package by.itacademy.eugeneyarosh.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Деление на 0 запрещено!");
+        }
+        return a / b;
+    }
+}
